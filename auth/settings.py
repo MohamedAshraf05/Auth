cd8ab08f48ff48dd3dbcb24123b2b3cf005eec52
M7md A4rf <ashourelsou8non@gmail.com>
2026-01-11 19:35:53 +0200

    pushing the database config

--- auth/settings.py
+++ auth/settings.py
@@ -94,10 +94,20 @@ WSGI_APPLICATION = 'auth.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'MoA4rf112005$default',
+        'USER': 'MoA4rf112005',
+        'PASSWORD': 'm7md_a4rf005',
+        'HOST': 'MoA4rf112005.mysql.pythonanywhere-services.com',
     }
 }
 
