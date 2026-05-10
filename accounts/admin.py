849e64a89e9b5229f964f53f27a48f9ed7827f4b
MohamedAshraf05 <ashourelsou8non@gmail.com>
2026-05-10 23:28:06 +0300

    fixing user profile admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import UserProfile , CustomUser
 # Register your models here.
 
-@admin.register(UserProfile)
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ["user" , "id" , "address"]
+# @admin.register(UserProfile)
+# class UserProfileAdmin(admin.ModelAdmin):
+#     list_display = ["user" , "id" , "address"]
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ["username" , "email" , "is_staff" , "is_active"]
